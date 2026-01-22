deff49978e98a7751c31aedd62094c6acd85a46e
hawai1401 <hawai1401@ik.me>
2026-01-22 21:24:33 +0100

    Ajout de la licence MIT, mise à jour de package.json pour inclure les fichiers et les exports, création d'un script post-build pour générer des fichiers CommonJS, et ajustements des options TypeScript.

---
+++ script/postbuild.js
@@ -0,0 +1,25 @@
+import fs from "fs";
+
+const dirs = fs
+  .readdirSync("./dist")
+  .filter((name) => !name.includes(".") && name !== "class");
+
+for (const dir of dirs) {
+  const subDirs = fs
+    .readdirSync(`./dist/${dir}`)
+    .filter((name) => !name.includes("."));
+
+  for (const subDir of subDirs) {
+    fs.createWriteStream(`./dist/${dir}/${subDir}/index.cjs`).write(
+      `"use strict";\nconst pkg = require("./index.js");\nmodule.exports = pkg;\n`,
+    );
+  }
+
+  fs.createWriteStream(`./dist/${dir}/index.cjs`).write(
+      `"use strict";\nconst pkg = require("./index.js");\nmodule.exports = pkg;\n`,
+    );
+}
+
+fs.createWriteStream("./dist/index.cjs").write(
+  `"use strict";\nconst pkg = require("./index.js");\nmodule.exports = pkg;\n`,
+);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { default as PterodactyleAPIClient } from "./class.js";
+export { PterodactyleAPIError } from "./class/APIError.js";
+export { default as HttpClient } from "./class/HttpClient.js";
 
 export * from "./types.js";
