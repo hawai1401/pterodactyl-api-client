79b7b5c3734f07e88282beaf108d10dc7f0dd4e7
hawai1401 <hawai1401@ik.me>
2026-04-09 19:56:07 +0200

    Builded pull request #1 from Whitedragon115/main

--- dist/application/nest/nest.client.d.ts
+++ dist/application/nest/nest.client.d.ts
@@ -1,8 +1,12 @@
 import type HttpClient from "../../class/HttpClient.js";
+import EggsClient from "./eggs/eggs.client.js";
+import EggClient from "./egg/egg.client.js";
 export default class NestClient {
     private httpClient;
+    eggs: EggsClient;
     readonly id: number;
     constructor(httpClient: HttpClient, id: number);
+    egg(id: number): EggClient;
     info(): Promise<{
         attributes: {
             created_at: Date;
--- dist/application/nest/nest.client.js
+++ dist/application/nest/nest.client.js
@@ -1,10 +1,17 @@
 import { nestId } from "./nest.schemas.js";
+import EggsClient from "./eggs/eggs.client.js";
+import EggClient from "./egg/egg.client.js";
 export default class NestClient {
     httpClient;
+    eggs;
     id;
     constructor(httpClient, id) {
         this.httpClient = httpClient;
         this.id = nestId.parse(id);
+        this.eggs = new EggsClient(httpClient, this.id);
+    }
+    egg(id) {
+        return new EggClient(this.httpClient, this.id, id);
     }
     async info() {
         const res = await this.httpClient.request("GET", `/application/nests/${this.id}`);
